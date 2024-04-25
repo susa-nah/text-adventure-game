a9dfb6f9e6447ab3ada880b066a1099f28b56406
susan-nah <susannahbourke@hotmail.com>
2024-04-25 14:51:11 +1000

    Project setup

---
+++ src/main/java/com/introtoprog/textadventuregame/TextAdventureGame.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.introtoprog.textadventuregame;
+
+/**
+ *
+ * @author susan
+ */
+public class TextAdventureGame {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

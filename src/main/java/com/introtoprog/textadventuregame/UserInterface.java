cff28c4df14c7c65f06722d044bd11852cdc24e1
susan-nah <susannahbourke@hotmail.com>
2024-04-30 19:54:42 +1000

    Refactored method to identify split index, and added method for parsing object. Matching unit tests.

--- src/main/java/com/introtoprog/textadventuregame/UserInterface.java
+++ src/main/java/com/introtoprog/textadventuregame/UserInterface.java
@@ -16,6 +16,40 @@ public class UserInterface {
         in = new Scanner(System.in);
     }
     
+    /*
+    Figure out where the command ends and the object name begins
+    */
+    public int getSplitIndex(String input) {
+        // Avoid NullPointerException
+        if (input == null || input.isEmpty() || input.isBlank()) {
+            return -1;
+        }
+        
+        // Normalise input
+        input = input.toUpperCase();
+        
+        // Conditions for leading text and length.
+        if (input.startsWith("LOOK INSIDE")) {
+            return "LOOK INSIDE ".length()-1;
+        } else if (input.startsWith("GO ")) { 
+            String minusGo = input.replace("GO ","");
+            if (minusGo.length() <=5) {
+                return input.length();
+            } else if (minusGo.contains(" ")) {
+                return minusGo.indexOf(" ") + 3;
+            } else {
+                return -1; // Not sure what to do here.
+            }
+        } else if (input.length() > 1) {
+            return input.indexOf(" ");
+        } else {
+            return 0;
+        }
+    }
+    
+    /*
+    Uses the split index to parse the command into standard forms.
+    */
     public String parseCommand(String input) {
         // Avoid NullPointerException
         if (input == null || input.isEmpty() || input.isBlank()) {
@@ -25,13 +59,16 @@ public class UserInterface {
         // Normalise input
         input = input.toUpperCase();
         
-        // Conditions for leading text and length.
-        if (input.startsWith("LOOK INSIDE")) {
-            return "LOOK INSIDE";
-        } else if (input.startsWith("GO ")) { 
+        // Use getSplitIndex() to determine start of command.
+        int splitIndex = getSplitIndex(input);
+        
+        if (splitIndex > 0) {
+            input = input.substring(0,splitIndex);
+        }
+        
+        // Handle Go direction commands
+        if (input.startsWith("GO ")) {
             input = input.replace("GO ","");
-        } else if (input.length() > 1) {
-            input = input.substring(0, input.indexOf(" "));
         }
         
         // Checks if command might be abbreviated to single char 
@@ -50,11 +87,56 @@ public class UserInterface {
                 return "WEST";
             case "L":
                 return "LOOK";
+            case "H":
+                return "HELP";
             default:
                 return input;
         }
         } else {
             return input;
         }
+    }
+     
+    /*
+    Cleans object part of string for comparison
+    */
+    public String parseObject(String input) {
+        // Avoid NullPointerException
+        if (input == null || input.isEmpty() || input.isBlank()) {
+            return null;
+        }
+        
+        // Get split index
+        int splitIndex = getSplitIndex(input);
+        
+        
+        // Return null for command only entries.
+        if (splitIndex == input.length() || splitIndex < 1) {
+            return null;
+        }
+        
+        // Normalise to uppercase.
+        input = input.toUpperCase();
+        
+        // If cardinal direction, return null;
+        if (splitIndex == 1 && 
+                !(input.startsWith("X")||input.startsWith("L"))) {
+            return null;
+        }
+        
+        // Get substring and trim whitespace
+        input = input.substring(splitIndex).trim();
+        
+        // Get rid of common leading text
+        String[] nonfiling = new String[] {"THE ","A ", "AN ","THAT ","THIS ",
+            "INSIDE ", "AT "};
+        for (String text : nonfiling) {
+            if (input.startsWith(text)) {
+                input = input.replace(text, "");
+            }
+        }
+        
+        return input;
+        
     }
 }
--- src/test/java/com/introtoprog/textadventuregame/TestUserInterface.java
+++ src/test/java/com/introtoprog/textadventuregame/TestUserInterface.java
@@ -17,15 +17,58 @@ public class TestUserInterface {
     UserInterface ui = new UserInterface();
     
     @ParameterizedTest
+    @CsvSource({"Look inside barrel,11", 
+        "Examine inside barrel,7", 
+        "X barrel,1", 
+        "go north,8",
+        "go east,7",
+        "go west east,7",
+        "n,0",
+        "look at barrel,4",
+        ",-1",
+        " ,-1",
+        "x white shirt,1"})
+    void testGetSplitIndex(String input, int expected) {
+        int output = ui.getSplitIndex(input);
+        assertEquals(expected, output);
+    }
+    
+    @ParameterizedTest
     @CsvSource({"Look inside barrel,LOOK INSIDE", 
         "Examine inside barrel,EXAMINE", 
         "X barrel,EXAMINE", 
         "go north,NORTH",
+        "go east,EAST",
+        "go west east,WEST",
+        "s,SOUTH",
         "n,NORTH",
         "look at barrel,LOOK",
-        ","})
+        ",",
+        " ,",
+        "xyz,XYZ",
+        "help,HELP",
+        "x white shirt,EXAMINE",})
     void testParseCommand(String input, String expected) {
         String output = ui.parseCommand(input);
+        assertEquals(expected, output);
+    }
+    
+    @ParameterizedTest
+    @CsvSource({"Look inside barrel,BARREL", 
+        "Examine inside barrel,BARREL", 
+        "X barrel,BARREL", 
+        "go north, ",
+        "go east, ",
+        "go west east,EAST",
+        "x white shirt,WHITE SHIRT",
+        "n,",
+        "look at barrel,BARREL",
+        ",",
+        " ,",
+        "xyz,",
+        "help me,ME"})
+    void testParseObject(String input, String expected) {
+        String output = ui.parseObject(input);
         assertEquals(expected, output);
     }
     
